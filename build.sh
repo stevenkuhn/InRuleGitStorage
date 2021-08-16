a0876dc736553b50defc67c0c401e9c8d565d41a
Steven Kuhn <email@stevenkuhn.net>
2021-08-15 19:00:30 -0500

    Fix build telemetry

--- build.sh
+++ build.sh
@@ -20,6 +20,9 @@ export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_MULTILEVEL_LOOKUP=0
 
+export DOTNET_NOLOGO=1
+export NUKE_TELEMETRY_OPTOUT=1
+
 ###########################################################################
 # EXECUTION
 ###########################################################################
